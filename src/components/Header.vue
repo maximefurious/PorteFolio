604405bba50af07d79adec7f053d5672c6091041
maximefurious <93995791+maximefurious@users.noreply.github.com>
2023-12-02 19:38:29 +0100

    [Add] - Add responsivness for home page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,58 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
+import {onMounted, onUnmounted, ref} from 'vue'
+
+
+function toggleMenu() {
+  document.querySelector('.menu-btn').classList.toggle('open')
+  document.querySelector('.menu').classList.toggle('open')
+}
+
+function closeMenu() {
+  document.querySelector('.menu-btn').classList.remove('open')
+  document.querySelector('.menu').classList.remove('open')
+}
+
+function handleMenuClick(e) {
+  console.log('handleMenuClick')
+  if (e.target.classList.contains('menu__nav-link')) {
+    closeMenu()
+  } else {
+    toggleMenu()
+  }
+}
+
+function handleEscKey(e) {
+  if (e.key === 'Escape') {
+    closeMenu()
+  }
+}
+
+function handleResize() {
+  if (window.innerWidth > 768) {
+    closeMenu()
+  }
+}
+
+onMounted(() => {
+  let menuBtn = document.querySelector('.menu-btn')
+  menuBtn.addEventListener('click', handleMenuClick)
+  window.addEventListener('keydown', handleEscKey)
+  window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+  let menuBtn = document.querySelector('.menu-btn')
+  menuBtn.removeEventListener('click', handleMenuClick)
+  window.removeEventListener('keydown', handleEscKey)
+  window.removeEventListener('resize', handleResize)
+})
+
+
 </script>
 
 <template>
-    <nav class="navbar">
+    <nav class="navbar" ref="navbar">
         <div class="left">
             <router-link to="/" class="nav-item">Portefolio.</router-link>
         </div>
@@ -16,6 +65,36 @@ import { RouterLink, RouterView } from 'vue-router'
             <router-link to="/contact" class="nav-item">Contact</router-link>
         </div>
     </nav>
+
+<!--  munu burger-->
+
+<div class="menu-btn" ref="menuBtn">
+    <div class="menu-btn__burger"></div>
+  </div>
+
+  <div class="menu">
+    <ul class="menu__nav">
+      <li class="menu__nav-item">
+        <a href="/" class="menu__nav-link">Accueil</a>
+      </li>
+      <li class="menu__nav-item">
+        <a href="/about" class="menu__nav-link">À propos</a>
+      </li>
+      <li class="menu__nav-item">
+        <a href="/projects" class="menu__nav-link">Projets</a>
+      </li>
+      <li class="menu__nav-item">
+        <a href="/skills" class="menu__nav-link">Compétences</a>
+      </li>
+      <li class="menu__nav-item">
+        <a href="/experiences" class="menu__nav-link">Expériences</a>
+      </li>
+      <li class="menu__nav-item">
+        <a href="/contact" class="menu__nav-link">Contact</a>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
@@ -61,5 +140,116 @@ export default {
 .navbar .right .nav-item {
     margin-left: 40px;
 }
+
+.menu-btn {
+    display: none;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    z-index: 1000;
+  }
+
+  .menu-btn__burger {
+    width: 50px;
+    height: 6px;
+    background-color: var(--text);
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(255, 101, 47, 0.2);
+    transition: all 0.3s ease;
+  }
+
+  .menu-btn__burger::before,
+  .menu-btn__burger::after {
+    content: "";
+    position: absolute;
+    width: 50px;
+    height: 6px;
+    background-color: var(--text);
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(255, 101, 47, 0.2);
+    transition: all 0.3s ease;
+  }
+
+  .menu-btn__burger::before {
+    transform: translateY(16px);
+  }
+
+  .menu-btn__burger::after {
+    transform: translateY(32px);
+  }
+
+  .menu-btn.open .menu-btn__burger {
+    transform: translate(-50px, 20px);
+    background-color: transparent;
+    box-shadow: none;
+  }
+
+  .menu-btn.open .menu-btn__burger::before {
+    transform: rotate(45deg) translate(35px, -35px);
+  }
+
+  .menu-btn.open .menu-btn__burger::after {
+    transform: rotate(-45deg) translate(35px, 35px);
+  }
+
+  .menu {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    clip-path: circle(0px at 100% -10%);
+    background-color: var(--primary);
+    transition: all 0.3s ease;
+    z-index: 999;
+  }
+
+  .menu.open {
+    clip-path: circle(1400px at 100% -10%);
+  }
+
+  .menu__nav {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 100%;
+  }
+
+  .menu__nav-item {
+    list-style: none;
+    margin-bottom: 20px;
+  }
+
+  .menu__nav-link {
+    text-decoration: none;
+    color: var(--text);
+    font-weight: bold;
+    font-size: 1.2rem;
+    transition: color 0.3s ease;
+  }
+
+  .menu__nav-link:hover {
+    color: var(--secondary);
+  }
+
+  @media screen and (max-width: 768px) {
+    .menu-btn {
+      display: flex;
+    }
+
+    .navbar .right {
+      display: none;
+    }
+
+    .home {
+      width: 100% !important;
+    }
+  }
+
 </style>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,7 @@ import ThreeCanvas from "@/components/ThreeCanvas.vue";
           Je suis un étudiant en informatique en <span class="school">BUT informatique</span> à Lannion. Dans la vie, j'aime l'adrénaline, les voitures et la programmation. Je suis quelqu'un de dynamique et
           je suis toujours à la recherche de nouveaux challenges, ainsi que d'opportunité pour m'améliorer et développer mes compétences.
         </p>
+        <a href="/contact" target="_blank" class="btn-contact">Me contacter</a>
       </div>
       <ThreeCanvas />
     </div>
@@ -54,6 +55,24 @@ import ThreeCanvas from "@/components/ThreeCanvas.vue";
 .home-paragrap .school {
   color: var(--primary);
   font-weight: bold;
+}
+
+.btn-contact {
+  padding: 10px 20px;
+  border-radius: 20px;
+  font-weight: bold;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: var(--text);
+  border: 2px solid var(--primary);
+  transition: all 0.3s ease;
+  margin-top: 20px !important;
+}
+
+.btn-contact:hover {
+  background-color: var(--primary);
+  color: var(--text);
+  transition: all 0.3s ease;
 }
 </style>
 
