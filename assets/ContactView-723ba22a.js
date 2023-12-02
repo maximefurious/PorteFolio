e0b7d14841918775a8c960b2f146a212b6b15444
maximefurious <93995791+maximefurious@users.noreply.github.com>
2023-12-02 17:05:00 +0000

    Build: (b479bd8) [Add] - Add planet for doing home page

---
+++ assets/ContactView-723ba22a.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{o as c,c as e,a as o}from"./index-382a27ed.js";const s={},a={class:"contact"},n=o("h1",null,"This is a contact page",-1),_=[n];function r(i,d){return c(),e("div",a,_)}const h=t(s,[["render",r]]);export{h as default};
