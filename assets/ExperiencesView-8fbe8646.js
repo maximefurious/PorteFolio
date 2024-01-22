c913d5384ba0526cd34b9602a39980211e5c2777
maximefurious <93995791+maximefurious@users.noreply.github.com>
2024-01-22 14:50:40 +0000

    Build: (495a6ba) [Update] Update project section

---
+++ assets/ExperiencesView-8fbe8646.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c,a as o}from"./index-bb667c45.js";const t={},n={class:"experiences"},r=o("h1",null,"experiences",-1),_=[r];function a(i,d){return s(),c("div",n,_)}const p=e(t,[["render",a]]);export{p as default};
