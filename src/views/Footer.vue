6cda903493aa2aeeccb5c688b7ceb166f4ad2c42
maximefurious <maxime.boujeant@gmail.com>
2024-06-03 23:34:44 +0200

    add/ Footer

---
+++ src/views/Footer.vue
@@ -0,0 +1,41 @@
+<script setup>
+  const year = new Date().getFullYear()
+</script>
+
+<template>
+    <footer class="footer">
+        <div class="footer__content">
+            <p>&copy; {{ year }} - Tous droits réservés</p>
+            <p>Créé par <a href="mailto:maxime.boujeant@gmail.com">Maxime Boujeant</a></p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    width: 100%;
+    background-color: var(--secondary);
+    color: var(--text);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+}
+
+.footer__content {
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.footer__content a {
+    color: var(--primary);
+}
+
+.footer__content a:hover {
+    color: var(--primary);
+    text-decoration: underline;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,118 +4,121 @@ import SocialLinkIcon from '@/components/SocialLinkIcon.vue'
 import ProjetsView from '@/views/ProjetsView.vue'
 import CompetencesView from './CompetencesView.vue'
 import ExperiencesView from '@/views/ExperiencesView.vue'
+import Footer from "@/views/Footer.vue";
+import router from "@/router";
 </script>
 
 <template>
-    <div class="home" id="about">
-        <div class="home-content">
-            <div class="home-text">
-                <h1 class="home-title">Hey,</h1>
-                <h2 class="home-subtitle">
-                    Je m'appelle <span>Maxime Boujeant</span>
-                </h2>
-                <p class="home-paragrap">
-                    Je suis un étudiant en
-                    <span class="school">BUT informatique</span> à Lannion. Dans
-                    la vie, j'aime l'adrénaline, les voitures et la
-                    programmation. Je suis quelqu'un de dynamique et je suis
-                    toujours à la recherche de nouveaux challenges, ainsi que
-                    d'opportunité pour m'améliorer et développer mes
-                    compétences.
-                </p>
-                <a href="/contact" class="btn-contact">Me contacter</a>
-                <div class="social-links">
-                    <social-link-icon
-                        link="https://www.linkedin.com/in/maxime-boujeant-88377a215/"
-                        icon="fab fa-linkedin"
-                        color="#0e76a8"
-                    />
-                    <social-link-icon
-                        link="https://github.com/maximefurious"
-                        icon="fab fa-github"
-                        color="#333"
-                    />
-                    <social-link-icon
-                        link="https://www.instagram.com/maximeboujeant/"
-                        icon="fab fa-instagram"
-                        color="#e1306c"
-                    />
-                </div>
-            </div>
-            <ThreeCanvas />
+  <div class="home" id="about">
+    <div class="home-content">
+      <div class="home-text">
+        <h1 class="home-title">Hey,</h1>
+        <h2 class="home-subtitle">
+          Je m'appelle <span>Maxime Boujeant</span>
+        </h2>
+        <p class="home-paragrap">
+          Je suis un étudiant en
+          <span class="school">BUT informatique</span> à Lannion. Dans
+          la vie, j'aime l'adrénaline, les voitures et la
+          programmation. Je suis quelqu'un de dynamique et je suis
+          toujours à la recherche de nouveaux challenges, ainsi que
+          d'opportunité pour m'améliorer et développer mes
+          compétences.
+        </p>
+        <a href="/contact" class="btn-contact">Me contacter</a>
+        <div class="social-links">
+          <social-link-icon
+              link="https://www.linkedin.com/in/maxime-boujeant-88377a215/"
+              icon="fab fa-linkedin"
+              color="#0e76a8"
+          />
+          <social-link-icon
+              link="https://github.com/maximefurious"
+              icon="fab fa-github"
+              color="#333"
+          />
+          <social-link-icon
+              link="https://www.instagram.com/maximeboujeant/"
+              icon="fab fa-instagram"
+              color="#e1306c"
+          />
         </div>
+      </div>
+      <ThreeCanvas/>
     </div>
-    <ProjetsView />
-    <CompetencesView />
-    <ExperiencesView />
+  </div>
+  <ProjetsView/>
+  <CompetencesView/>
+  <ExperiencesView/>
+  <Footer/>
 </template>
 
 <style>
 .home {
-    width: 60%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    padding: 20px;
+  width: 60%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding: 20px;
 }
 
 .home-title {
-    font-weight: bold;
-    font-size: 3rem;
-    margin-bottom: 10px;
-    color: var(--primary);
+  font-weight: bold;
+  font-size: 3rem;
+  margin-bottom: 10px;
+  color: var(--primary);
 }
 
 .home-subtitle {
-    font-weight: bold;
-    font-size: 2rem;
-    margin-bottom: 10px;
-    color: var(--text);
+  font-weight: bold;
+  font-size: 2rem;
+  margin-bottom: 10px;
+  color: var(--text);
 }
 
 .home-subtitle span {
-    color: var(--primary);
-    font-weight: bolder;
+  color: var(--primary);
+  font-weight: bolder;
 }
 
 .home-paragrap {
-    font-size: 1.2rem;
-    color: var(--text);
-    max-width: 800px;
+  font-size: 1.2rem;
+  color: var(--text);
+  max-width: 800px;
 }
 
 .home-paragrap .school {
-    color: var(--primary);
-    font-weight: bold;
+  color: var(--primary);
+  font-weight: bold;
 }
 
 .btn-contact {
-    padding: 10px 20px;
-    border-radius: 20px;
-    font-weight: bold;
-    font-size: 1.2rem;
-    text-decoration: none;
-    color: var(--text);
-    border: 2px solid var(--primary);
-    transition: all 0.3s ease;
-    margin-top: 20px !important;
+  padding: 10px 20px;
+  border-radius: 20px;
+  font-weight: bold;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: var(--text);
+  border: 2px solid var(--primary);
+  transition: all 0.3s ease;
+  margin-top: 20px !important;
 }
 
 .btn-contact:hover {
-    background-color: var(--primary);
-    color: var(--text);
-    transition: all 0.3s ease;
+  background-color: var(--primary);
+  color: var(--text);
+  transition: all 0.3s ease;
 }
 
 .social-links {
-    display: flex;
-    align-items: center;
-    margin-top: 20px;
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
 }
 
 @media screen and (max-width: 768px) {
-    .home {
-        width: 100% !important;
-    }
+  .home {
+    width: 100% !important;
+  }
 }
 </style>
